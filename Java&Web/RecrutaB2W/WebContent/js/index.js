c27cffc035fae89b122fa41765ae664ca9c32354
Eduardo <eduardo.benigno15@gmail.com>
2020-09-12 18:40:49 -0300

    dando uma limpeza

--- Java&Web/RecrutaB2W/WebContent/js/index.js
+++ Java&Web/RecrutaB2W/WebContent/js/index.js
@@ -1,71 +1,3 @@
-console.log("foi")
-
-document.addEventListener('DOMContentLoaded',init);
-
-function init(){
-  const txtElement = document.querySelector('.txt-type');
-  const words = JSON.parse(txtElement.getAttribute('data-words'));
-  const wait = txtElement.getAttribute('data-wait');
-
-  new TypeWriter(txtElement,words,wait);
-}
-
-const TypeWriter = function(txtElement,words,wait = 3000){
-  this.txtElement = txtElement;
-  this.words = words;
-  this.txt = "";
-  this.wordIndex = 0;
-  this.wait = parseInt(wait,10);
-  this.type();
-  this.isDeleting = false;
-}
-
-TypeWriter.prototype.type = function(){
-  
-  const current = this.wordIndex % this.words.length;
-
-  const fulltxt = this.words[current];
-  const last = this.words[this.words.length-1];
-
-  if(this.isDeleting){
-    this.txt = fulltxt.substring(0,this.txt.length-1)
-  }
-
-  else{
-    this.txt = fulltxt.substring(0,this.txt.length+1)
-  }
-
-
-  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-  let typeSpeed = 100;
-
-  if(this.isDeleting){
-    typeSpeed /= 2; 
-  }
-
-  if(!this.isDeleting && this.txt === fulltxt){
-    typeSpeed = this.wait;
-    this.isDeleting = true;
-  }
-
-  else if(this.isDeleting && this.txt ===''){ 
-    this.isDeleting = false;
-    this.wordIndex++;
-    typeSpeed = 200; //velocidade de digitação
-  }  
-
-  setTimeout(() => this.type(), typeSpeed); 
-}
-
-$(".carousel").slick({
-  lazyLoad: 'ondemand',
-  dots:true,
-  arrows:true,
-  autoplay:5000,
-  infinite:true
-})
-
 const mobileButton = document.querySelector('.mobile-btn');
 mobileButton.onclick = function() {
   this.classList.toggle('active');
@@ -83,7 +15,7 @@ voltarTopo.addEventListener("click", function(){
 })
 
 var scrollBtn = $(".c-scroll-up")
-var formOffset = $("form").offset().top
+// var formOffset = $("form").offset().top
 var formHeight = $("form").innerHeight()
 
 
@@ -116,34 +48,4 @@ function scrollToIdOnClick(e){
     behavior:"smooth",
   });
 }
-
-let btn_email = document.getElementById("#btn-email")
-
-btn_email.addEventListener("click", 
-function enviarEmail(e){
-
-  e.preventDefault();
-
-  let nome = document.querySelector("#nome").value;
-  let email = document.querySelector("#email").value;
-  let telefone = document.querySelector("#telefone").value;
-  let assunto = document.querySelector("#assunto").value;
-  let mensagem = document.querySelector("#mensagem").value;
-
-  let nossoEmail = "oode.awny@gmail.com";
-
-
-  Email.send({
-
-    SecureToken : "a31a0d3c-67d7-4ac2-95a1-1d09e2971a1b",
-    // a31a0d3c-67d7-4ac2-95a1-1d09e2971a1b
-    To : nossoEmail,
-    From : nossoEmail,
-    Subject : assunto,
-    Body : mensagem
-    }).then(
-      message => alert("E-mail enviado, obrigado!"),
-                console.log(nossoEmail,assunto,mensagem)
-  );
-})
 
