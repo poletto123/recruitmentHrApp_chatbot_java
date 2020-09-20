54a191d2b54631a5278d3d2b532e3faaabdafd57
Eduardo <eduardo.benigno15@gmail.com>
2020-09-20 15:18:07 -0300

    ajustes visuais e alguns resposnivos

--- Java&Web/RecrutaB2W/WebContent/js/index.js
+++ Java&Web/RecrutaB2W/WebContent/js/index.js
@@ -1,3 +1,61 @@
+document.addEventListener('DOMContentLoaded',init);
+
+function init(){
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+
+  new TypeWriter(txtElement,words,wait);
+}
+
+const TypeWriter = function(txtElement,words,wait = 3000){
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait,10);
+  this.type();
+  this.isDeleting = false;
+}
+
+TypeWriter.prototype.type = function(){
+  
+  const current = this.wordIndex % this.words.length;
+
+  const fulltxt = this.words[current];
+  const last = this.words[this.words.length-1];
+
+  if(this.isDeleting){
+    this.txt = fulltxt.substring(0,this.txt.length-1)
+  }
+
+  else{
+    this.txt = fulltxt.substring(0,this.txt.length+1)
+  }
+
+
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  let typeSpeed = 100;
+
+  if(this.isDeleting){
+    typeSpeed /= 2; 
+  }
+
+  if(!this.isDeleting && this.txt === fulltxt){
+    typeSpeed = this.wait;
+    this.isDeleting = true;
+  }
+
+  else if(this.isDeleting && this.txt ===''){ 
+    this.isDeleting = false;
+    this.wordIndex++;
+    typeSpeed = 200; //velocidade de digitação
+  }  
+
+  setTimeout(() => this.type(), typeSpeed); 
+}
+
 const mobileButton = document.querySelector('.mobile-btn');
 mobileButton.onclick = function() {
   this.classList.toggle('active');
@@ -15,7 +73,7 @@ voltarTopo.addEventListener("click", function(){
 })
 
 var scrollBtn = $(".c-scroll-up")
-// var formOffset = $("form").offset().top
+var formOffset = $("form").offset().top
 var formHeight = $("form").innerHeight()
 
 
@@ -49,3 +107,21 @@ function scrollToIdOnClick(e){
   });
 }
 
+
+const btnLeiaMais = document.querySelectorAll('.content-infos__text--button');
+
+function leiamais() {
+  this.previousElementSibling.classList.toggle("active");
+
+  if(this.previousElementSibling.classList.contains("active")){
+    this.innerText = "Leia menos"
+  }
+
+  else{
+    this.innerText = "Leia mais"
+  }
+}
+
+btnLeiaMais.forEach((i) => {
+  i.addEventListener('click', leiamais);
+});
